6a35e13de2c28516cb4bef84fe236046bc028d76
Yourockit <sts2ro@gmail.com>
2022-12-25 21:50:57 +0300

    hw-js-06-task-2

--- my-js/my-hw-6/js/task-02.js
+++ my-js/my-hw-6/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
 ];
+
+const ingredientLi = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+    const liEl = document.createElement('li');
+    liEl.classList.add('.item');
+    liEl.textContent = ingredient;
+
+    return liEl;
+});
+
+ingredientLi.append(...elements);
