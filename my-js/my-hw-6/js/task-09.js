625032b18eec2ba23e58d71575b7caec0c272e62
Yourockit <sts2ro@gmail.com>
2022-12-28 02:05:10 +0300

    js-hw-06-task-9

--- my-js/my-hw-6/js/task-09.js
+++ my-js/my-hw-6/js/task-09.js
@@ -1,3 +1,14 @@
+const button = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+button.addEventListener('click', onButtonChangeColor);
+
+function onButtonChangeColor(event) {
+    const newColor = getRandomHexColor();
+    document.body.style.background = `${newColor}`;
+    span.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
