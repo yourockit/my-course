5dab325c0e83f767d0a0c443903e6dadcc37fe37
Yourockit <sts2ro@gmail.com>
2022-12-28 01:00:08 +0300

    js-hw-06-task-8

--- my-js/my-hw-6/js/task-08.js
+++ my-js/my-hw-6/js/task-08.js
@@ -1,1 +1,32 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    // const formData = new FormData(event.currentTarget);
+
+    // formData.forEach((value, name) => {
+    //     if (value === "") {
+    //         alert(`Не все поля формы заполнены`);
+    //     }
+    //     console.log(name);
+    //     console.log(value);
+    // });
+
+    const formElement = event.currentTarget.elements;
+    const email = formElement.email.value;
+    const password = formElement.password.value;
+    if (email === "" || password === "") {
+        alert(`Не все поля формы заполнены`);
+    } else {
+        const formValue = {
+            email,
+            password,
+        };
+
+        console.log(formValue);
+        event.currentTarget.reset();
+    };
+};
