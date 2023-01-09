451b396be0b4f1eed6cfae6680d2816ad528362b
Yourockit <sts2ro@gmail.com>
2023-01-09 21:09:26 +0300

    js-hw-07-2

--- my-js/my-hw-7/js/02-lightbox.js
+++ my-js/my-hw-7/js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCardMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+// gallery.addEventListener('click', onGalleryCardClick);
+
+function createGalleryCardMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" />
+        </a>`
+    }).join('');
+};
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
