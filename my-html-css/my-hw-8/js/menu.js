219f02df3c76dd86b9d5a4b7293409046bf580c6
y.ostriianskyi <ostriianskyiyurii@gmail.com>
2024-02-13 14:47:07 +0100

    loader

--- my-html-css/my-hw-8/js/menu.js
+++ my-html-css/my-hw-8/js/menu.js
@@ -1,18 +1,34 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-    const menuBodyRef = document.querySelector("[data-menu-body");
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const menuBodyRef = document.querySelector("[data-menu-body");
 
-    menuBtnRef.addEventListener("click", () => {
-        const expanded =
-            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-        menuBtnRef.classList.toggle("is-open");
-        menuBtnRef.setAttribute("aria-expanded", !expanded);
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-        mobileMenuRef.classList.toggle("is-open");
+    mobileMenuRef.classList.toggle("is-open");
 
-        menuBodyRef.classList.toggle("menu-open");
+    menuBodyRef.classList.toggle("menu-open");
+  });
+})();
 
-    })
+(() => {
+  const targetLink = document.querySelector("[data-link]");
+  const link = targetLink.dataset.link;
+
+  targetLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.body.innerHTML = `<div class="loader">
+    <div class="loader-inner">
+        <img src="/images/loader-volvo.gif"/>
+    </div>
+</div>`;
+    setTimeout(() => {
+      window.location.href = link;
+    }, 2000);
+  });
 })();
