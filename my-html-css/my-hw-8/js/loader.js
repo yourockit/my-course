211087d8ebd269527838e26b8046b6e5184c9cea
y.ostriianskyi <ostriianskyiyurii@gmail.com>
2024-02-13 14:56:18 +0100

    loader

---
+++ my-html-css/my-hw-8/js/loader.js
@@ -0,0 +1,16 @@
+(() => {
+  const targetLink = document.querySelector("[data-link]");
+  const link = targetLink.dataset.link;
+
+  targetLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.body.innerHTML = `<div class="loader">
+      <div class="loader-inner">
+          <img src="/images/loader-volvo.gif"/>
+      </div>
+  </div>`;
+    setTimeout(() => {
+      window.location.href = link;
+    }, 2000);
+  });
+})();
--- my-html-css/my-hw-8/js/menu.js
+++ my-html-css/my-hw-8/js/menu.js
@@ -15,20 +15,3 @@
     menuBodyRef.classList.toggle("menu-open");
   });
 })();
-
-(() => {
-  const targetLink = document.querySelector("[data-link]");
-  const link = targetLink.dataset.link;
-
-  targetLink.addEventListener("click", (e) => {
-    e.preventDefault();
-    document.body.innerHTML = `<div class="loader">
-    <div class="loader-inner">
-        <img src="/images/loader-volvo.gif"/>
-    </div>
-</div>`;
-    setTimeout(() => {
-      window.location.href = link;
-    }, 2000);
-  });
-})();
